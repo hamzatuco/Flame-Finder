f12fc215365928465cfb19e4f0fd85bd9663e51f
Hamza <htuco04@gmail.com>
2023-04-07 04:01:53 +0200

    first commit

---
+++ lib/Pages/HomePage.dart
@@ -0,0 +1,170 @@
+// ignore_for_file: file_names, unused_import
+
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          Positioned.fill(
+            child: Image.asset(
+              'assets/pozadina.jpg',
+              fit: BoxFit.cover,
+              alignment: const Alignment(0.9, 0.5),
+            ),
+          ),
+          Positioned(
+            top: 70, // adjust the value here to move the text down
+            left: 25,
+            right: 25,
+            child: Column(
+              children: [
+                FittedBox(
+                  fit: BoxFit.scaleDown,
+                  child: Text(
+                    'Flame Finder',
+                    textAlign: TextAlign.center,
+                    overflow: TextOverflow.ellipsis,
+                    style: GoogleFonts.poppins(
+                      color: Colors.white,
+                      fontSize: 29,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+                const Padding(
+                  padding: EdgeInsets.fromLTRB(0, 10, 0, 0),
+                  child: SizedBox(
+                    width: 30.0,
+                    child: Divider(
+                      color: Colors.white,
+                      thickness: 1.0,
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(25, 15, 25, 0),
+                  child: Text('Prijavite ili pretražujte lokalne požare',
+                      textAlign: TextAlign.center,
+                      overflow: TextOverflow.ellipsis,
+                      style: GoogleFonts.poppins(
+                          color: Colors.white,
+                          fontSize: 15,
+                          fontWeight: FontWeight.w500)),
+                ),
+              ],
+            ),
+          ),
+          Positioned(
+            bottom: 40,
+            left: 0,
+            right: 0,
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                SizedBox(
+                  height: 65, //height of button
+                  width: 240,
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.white,
+                      textStyle: const TextStyle(
+                        color: Colors.black,
+                        fontSize: 17,
+                        fontStyle: FontStyle.normal,
+                      ),
+                    ),
+                    onPressed: () {},
+                    child: Row(
+                      children: [
+                        Image.asset(
+                          'assets/email.png',
+                          height: 37,
+                          width: 37,
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(left: 16),
+                          child: Text(
+                            'Email prijava',
+                            textAlign: TextAlign.center,
+                            overflow: TextOverflow.ellipsis,
+                            style: GoogleFonts.poppins(
+                              color: Colors.black,
+                              fontSize: 16,
+                              fontWeight: FontWeight.w600,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 15),
+                SizedBox(
+                  height: 65, //height of button
+                  width: 240,
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.white,
+                      textStyle: const TextStyle(
+                        color: Colors.black,
+                        fontSize: 17,
+                        fontStyle: FontStyle.normal,
+                      ),
+                    ),
+                    onPressed: () {},
+                    child: Row(
+                      children: [
+                        Image.asset(
+                          'assets/google.png',
+                          height: 29,
+                          width: 29,
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(left: 20),
+                          child: Text(
+                            'Google prijava',
+                            textAlign: TextAlign.center,
+                            overflow: TextOverflow.ellipsis,
+                            style: GoogleFonts.poppins(
+                              color: Colors.black,
+                              fontSize: 16,
+                              fontWeight: FontWeight.w600,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 35),
+                  child: Text(
+                    'Napravite račun >',
+                    textAlign: TextAlign.center,
+                    overflow: TextOverflow.ellipsis,
+                    style: GoogleFonts.poppins(
+                      color: Colors.white,
+                      fontSize: 15,
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
