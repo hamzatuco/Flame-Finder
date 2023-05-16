ada4a833922894b45e5f6d973d0bb05766c8aa99
hamzatuco <htuco04@gmail.com>
2023-05-16 21:52:18 +0200

    Misc

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,4 @@
 import 'package:flamefinder/Pages/Dashboard.dart';
-import 'package:flamefinder/Pages/HomePage.dart';
-import 'package:flamefinder/Pages/PozarSplash.dart';
-import 'package:flamefinder/Pages/Prijava.dart';
-import 'package:flamefinder/Pages/SplashVolonter.dart';
-import 'package:flamefinder/Pages/SprijeciPozar.dart';
-import 'Pages/SplashVolonter.dart';
-import 'Pages/SplashPrijava.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -15,11 +8,10 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: SplashVolonter(),
+      home: Dashboard(),
       debugShowCheckedModeBanner: false,
     );
   }
